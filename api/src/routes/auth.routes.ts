00c34022460db4579e78088916d7952a3164768a
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-02 14:49:01 -0300

    data validation in register

--- api/src/routes/auth.routes.ts
+++ api/src/routes/auth.routes.ts
@@ -5,17 +5,36 @@ import UserSchema from '../models/User'
 
 const routes = Router()
 
+type registerUser = {
+  name: string
+  email: string
+  password: string
+  confirmPassword: string
+}
+
 routes.post('/register', async (req, res) => {
   const {
     name,
     email,
     password,
     confirmPassword
-  } = req.body
+  } : registerUser = req.body
 
-  if (name === null || email === null || password === null || confirmPassword === null) {
-    return res.status(400).json({ error: 'Por favor, preencha todos os campos !' })
-  }
+  // Check for required fields
+  if (name === undefined || email === undefined || password === undefined || confirmPassword === undefined) return res.status(400).json({ error: 'Por favor, preencha todos os campos !' })
+
+  // Check if passwords match
+  if (password !== confirmPassword) return res.status(400).json({ error: 'As senhas não conferem !' })
+
+  // Check if user exists
+  const emailExist = await UserSchema.findOne({ email })
+  if (emailExist) return res.status(400).json({ error: 'O e-mail informado já esta em uso !' })
+
+  // Create password
+  const salt = await bcript.genSalt(12)
+  const passwordHash = await bcript.hash(password, salt)
+
+  res.status(400).json({ error: 'O e-mail informado já esta em uso !' })
 })
 
 export default routes
