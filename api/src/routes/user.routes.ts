dc37d1c4d082cae6472604eccc15ecea009a0b7e
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-03 21:57:24 -0300

    add token validate

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import UserController from '../controllers/User'
+import checkToken from '../helpers/check-token'
 
 const routes = Router()
 
-routes.get('/:id', UserController.getUserById)
+routes.get('/:id', checkToken, UserController.getUserById)
 
 export default routes
