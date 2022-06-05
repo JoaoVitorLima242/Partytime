4360b30414e2ff03d1a86decb8cc9b9a2659f2a5
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-04 21:32:40 -0300

    add return to res

--- api/src/controllers/Party.ts
+++ api/src/controllers/Party.ts
@@ -58,10 +58,9 @@ class PartyControllers {
     try {
       const parties = await PartySchema.find({ privacy: false }).sort([['_id', -1]])
 
-      console.log(parties)
-      res.json({ error: null, parties })
+      return res.json({ error: null, parties })
     } catch (error) {
-      res.status(400).json(error)
+      return res.status(400).json(error)
     }
   }
 }
