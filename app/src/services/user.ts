33f9688423b9241b8b790292bbe4019e593ac870
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-09 22:01:59 -0300

    add getUserById

--- app/src/services/user.ts
+++ app/src/services/user.ts
@@ -1,9 +1,18 @@
 import { RegisterData, RegisterUserType } from 'contexts/Auth/AuthContext.d'
 import { api } from './axios'
 
-export const RegisterRequest = async (data: RegisterData): Promise<RegisterUserType> => {
+export const registerRequest = async (data: RegisterData): Promise<RegisterUserType> => {
   try {
     const response = await api.post('/api/auth/register', data)
+    return response.data
+  } catch (error) {
+    return error.response.data
+  }
+}
+
+export const getUserById = async (id: string) => {
+  try {
+    const response = await api.post(`/api/user/${id}`)
     return response.data
   } catch (error) {
     return error.response.data
